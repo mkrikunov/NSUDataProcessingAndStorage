58dac3884a3a2426302def168489ae87b2d7bae9
Mikhail <misha_k_2018@mail.ru>
2024-06-04 14:48:44 +0700

    edit D5-D6 tasks

--- tasks_D5-D6/app/database.py
+++ tasks_D5-D6/app/database.py
@@ -1,5 +1,5 @@
 from databases import Database
 
-DATABASE_URL = "postgresql://postgres:tulula98@localhost:5432/demo"
+DATABASE_URL = "postgresql://username:password@localhost:5432/demo"
 
 database = Database(DATABASE_URL)
